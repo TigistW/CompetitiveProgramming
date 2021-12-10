2d2c568ebd369224fd00824283a021a4dbf0d3a6
TigistW <wondimnehtigist@gmail.com>
2021-12-10 18:22:33 +0300

    solving insertion

---
+++ Week1/insertion.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/insertionsort1/problem
+
+def insertionSort1(n, arr):
+    for i in range(n):
+        current=arr[i]
+        
+        j=i-1
+        while (j>=0 and arr[j]>current):
+            arr[j+1]=arr[j];
+            j=j-1;
+            print(arr)
+        arr[j+1]=current
+        
+    print(arr)
+    
