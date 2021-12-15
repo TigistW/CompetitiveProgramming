ccef6f4fb57e806ab94075e436e5a9434b6ac250
TigistW <88367517+TigistW@users.noreply.github.com>
2021-12-15 22:16:47 +0300

    Time: 101 ms (24.03%), Space: 16.1 MB (84.25%) - LeetHub

---
+++ merge-intervals/merge-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals):
+        length = len(intervals)
+        intervals.sort()
+        
+        i = 0
+        while (i <length-1):
+            j=i+1
+            if(intervals[i][1] >= intervals[j][0]):
+                if(intervals[i][1] < intervals[j][1]):
+                    intervals[i][1] = intervals[j][1]
+                intervals.pop(j)
+                i =i-1
+                length=length-1
+                
+            i=i+1
+        return intervals
+                
+        
