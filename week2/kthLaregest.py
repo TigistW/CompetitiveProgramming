f27868e2e709f1b7cddb3103837e19ce0321cc10
TigistW <wondimnehtigist@gmail.com>
2021-12-17 19:32:12 +0300

    kth largest

---
+++ week2/kthLaregest.py
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums.sort(reverse=True,key=lambda x: int(x))
+        return nums[k-1]
