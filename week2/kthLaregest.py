4e5f0fbed80540e54a76f7a98b4353ab7b2a8791
TigistW <88367517+TigistW@users.noreply.github.com>
2021-12-17 23:38:16 +0300

    Update kthLaregest.py

--- week2/kthLaregest.py
+++ week2/kthLaregest.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
+#https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
         nums.sort(reverse=True,key=lambda x: int(x))
