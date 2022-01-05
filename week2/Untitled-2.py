0c9d0f1231ac5050478350af7bc49a36bfe3bd5a
TigistW <wondimnehtigist@gmail.com>
2022-01-05 09:15:23 +0300

    more problems

---
+++ week2/Untitled-2.py
@@ -0,0 +1,34 @@
+def fib(n):
+    if n>1:
+        return fib(n-1)+fib(n-2)
+    else:
+        if n==0:
+            return 0
+
+        else:
+            if n==1:
+                return 1
+fib(3)
+
+def winner(nums):
+    score1=0
+    score2=0
+    num=0
+    if p==1:
+        p1=nums[e]
+        tp1 + tp2=f(s, e-1, 2)   #call the second player
+        curr=[p1+tp1,tp2]
+
+        p1 = nums[s]
+        tp1 + tp2 = f(s, e-1, 2)  # call the second player
+        curr = [p1+tp1, tp2]
+
+        
+
+
+
+
+    
+    
+    
+    
