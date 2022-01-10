559cbac4949ac5f8f726f1c82662bfc3bbeb37ca
TigistW <wondimnehtigist@gmail.com>
2022-01-10 22:08:42 +0300

    is the power of three

---
+++ week3/isPowerOfThree.py
@@ -0,0 +1,10 @@
+#https: // leetcode.com/problems/power-of-three/
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        if n % 3 != 0 or n == 0:
+            return False
+
+        return self.isPowerOfThree(n//3)
