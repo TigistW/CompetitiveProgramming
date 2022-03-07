90592d88bbb54e5681ab3fe2e2fb5753b57d6d46
TigistW <88367517+TigistW@users.noreply.github.com>
2022-03-07 16:18:55 +0300

    Time: 1044 ms (48.11%), Space: 32.4 MB (26.51%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        def validIdx(idx):
+            return idx < len(arr) and idx >= 0 and idx not in vi
+        
+        duplicate = dict()
+        for i in range(len(arr)):
+            if arr[i] not in duplicate:
+                duplicate[arr[i]] = [i]
+            else:
+                duplicate[arr[i]].append(i)
+                
+        visited = set()
+        vi= set()
+        q = deque([0])
+        count = 0
+        while q:
+            length_q = len(q)
+            for i in range(length_q):
+                popped = q.popleft()
+                vi.add(popped)
+                if popped == len(arr)-1:
+                    return count
+                for i in [-1,1]:   
+                    if validIdx(popped+i):
+                        q.append(popped+i)
+                        
+                if arr[popped] in visited:
+                    continue
+                visited.add(arr[popped])
+                
+                for j in duplicate[arr[popped]]:
+                    if validIdx(j):
+                        q.append(j)      
+            count += 1
+        return count
+        
+        
