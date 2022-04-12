294948151db283ddb0ed38018f1d6fa7b08c0856
TigistW <88367517+TigistW@users.noreply.github.com>
2022-04-12 10:59:00 +0300

    Time: 131 ms (60.96%), Space: 15.8 MB (47.91%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        courses = defaultdict(set)
+        pres = defaultdict(set)
+        res = []
+        for course, pre in prerequisites:
+            courses[course].add(pre)
+            pres[pre].add(course)
+        stack=[n for n in range(numCourses) if not courses[n]]
+        count = 0
+        while stack:
+            no_pre = stack.pop()
+            res.append(no_pre)
+            count+=1
+            for course in pres[no_pre]:
+                courses[course].remove(no_pre)
+                if not courses[course]:
+                    stack.append(course)
+        return res if count==numCourses else []
