168b4e6e87f343bb2dc197cf2dc7a16bb83e7226
TigistW <88367517+TigistW@users.noreply.github.com>
2022-04-13 22:44:47 +0300

    Time: 70 ms (82.31%), Space: 15.7 MB (9.83%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Set = set()
+        idx = dict()
+        diff = 0
+        res = []
+        
+        for i in range(len(nums)):
+            Set.add(nums[i])
+            
+            
+        for i in range(len(nums)):
+            idx[nums[i]] = i
+            
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in Set and (idx[diff] != i):
+                res.append(i)
+                res.append(idx[diff]) 
+                break
+        return res
+                
+                
+            
