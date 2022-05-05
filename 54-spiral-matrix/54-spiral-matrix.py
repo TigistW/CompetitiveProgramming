c9ee3caddbef59ea5e8a8f00d721f17114704b05
TigistW <88367517+TigistW@users.noreply.github.com>
2022-05-05 14:37:13 +0300

    Time: 50 ms (27.64%), Space: 14 MB (34.30%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -2,14 +2,10 @@ class Solution:
   def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
     if not matrix:
       return []
-
-    m = len(matrix)
-    n = len(matrix[0])
+    m,n = len(matrix),len(matrix[0])
     ans = []
-    r1 = 0
-    c1 = 0
-    r2 = m - 1
-    c2 = n - 1
+    r1,c1,r2,c2 = 0,0,m-1,n-1
+    
     while len(ans) < m * n:
       j = c1
       while j <= c2 and len(ans) < m * n:
