d0a59389fac4aff34d88bba2f9634e11c783c58f
TigistW <88367517+TigistW@users.noreply.github.com>
2022-05-16 17:18:57 +0300

    Time: 30 ms (87.74%), Space: 13.9 MB (64.23%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        kMax = 3000
+        while b:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        return a if a < kMax else ~(a ^ mask)
+
+        
+
+          
+        
