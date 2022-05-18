5f979cddb46fe8687a6faf65c7ffac861cc7726b
TigistW <88367517+TigistW@users.noreply.github.com>
2022-05-18 18:56:10 +0300

    Time: 1636 ms (38.63%), Space: 13.9 MB (91.91%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -2,26 +2,25 @@ class Solution:
   def longestPalindrome(self, s: str) -> str:
     if not s:
       return ''
-
-    def extend(s: str, i: int, j: int) -> Tuple[int, int]:
+    def outward(s,i,j):
       while i >= 0 and j < len(s):
         if s[i] != s[j]:
           break
         i -= 1
         j += 1
       return i + 1, j - 1
-
     indices = [0, 0]
-
     for i in range(len(s)):
-      l1, r1 = extend(s, i, i)
-      if r1 - l1 > indices[1] - indices[0]:
-        indices = l1, r1
+      left, right = outward(s, i, i)
+      if right - left > indices[1] - indices[0]:
+        indices = left, right
       if i + 1 < len(s) and s[i] == s[i + 1]:
-        l2, r2 = extend(s, i, i + 1)
-        if r2 - l2 > indices[1] - indices[0]:
-          indices = l2, r2
-
+        left2, right2 = outward(s, i, i + 1)
+        if right2 - left2 > indices[1] - indices[0]:
+          indices = left2, right2
+          
+      # print(left,right) 
+      # print(s[indices[0]:indices[1] + 1])
     return s[indices[0]:indices[1] + 1]
 
         
