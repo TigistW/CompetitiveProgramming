3b9785e57ef7c02926df84615ad133bfb28b18d2
TigistW <88367517+TigistW@users.noreply.github.com>
2022-07-17 02:03:24 +0300

    Time: 93 ms (80.17%), Space: 14.1 MB (58.66%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+
+        def dfs(s,target,path):
+          
+          if target < 0:
+            return
+          
+          if target == 0:
+            ans.append(path.copy())
+            return
+
+          for i in range(s, len(candidates)):
+            path.append(candidates[i])
+            dfs(i, target - candidates[i], path)
+            path.pop()
+
+        candidates.sort()
+        dfs(0, target, [])
+        return ans
