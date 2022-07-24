6891777f3735e8f2904fceff2129bf04c1822d9a
TigistW <88367517+TigistW@users.noreply.github.com>
2022-07-24 14:59:56 +0300

    Time: 173 ms (50.55%), Space: 15.2 MB (54.98%) - LeetHub

---
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        ans = [[rStart, cStart]]
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+        i = 0
+        while len(ans) < rows * cols:
+          for _ in range(i // 2 + 1):
+            rStart += dy[i % 4]
+            cStart += dx[i % 4]
+            if 0 <= rStart < rows and 0 <= cStart < cols:
+              ans.append([rStart, cStart])
+          i += 1
+        return ans
