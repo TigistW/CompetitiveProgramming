d4f176a796e519ee05235e53bcc7cef2b7679c32
TigistW <88367517+TigistW@users.noreply.github.com>
2022-08-16 11:42:18 +0300

    Time: 324 ms (32.52%), Space: 31.5 MB (72.93%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,42 +1,52 @@
-class TrieNode:
-    def __init__(self, letter = ""):
-        self.letter = letter
-        self.children = {}
-        self.mark = False
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for letter in word:
-            if letter in node.children:
-                node = node.children[letter]
-            else:
-                new_node = TrieNode(letter)
-                node.children[letter] = new_node
-                node = new_node
-        node.mark = True
-        
-    def search(self, word: str) -> bool:
-        node = self.root
-        for letter in word:
-            if letter not in node.children:
-                return False
-            node = node.children[letter]
-        return node.mark
-        
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for letter in prefix:
-            if letter not in node.children:
-                return False
-            node = node.children[letter]
-        return True
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
+
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+class Node:
+  def __init__(self, let = ""):
+    self.let = let
+    self.end = False
+    self.children = {}
+    
+class Trie:
+  def __init__(self):
+    self.root = Node()
+    
+  def insert(self, word):
+    cur = self.root 
+    for i in word: 
+      if i not in cur.children:
+         cur.children[i] = Node()
+      cur  = cur.children[i]
+    cur.end = True
+  
+  def search(self, word):
+    cur = self.root
+
+    for i in word: 
+      if i not in cur.children:
+         # cur.children[i] = Node()
+          return False
+      cur  = cur.children[i]
+
+    if cur.end:
+      return True
+    return False
+    
+  def startsWith(self, word):
+    cur = self.root
+
+    for i in word: 
+      if i not in cur.children:
+          return False
+      cur  = cur.children[i]
+    return True
+
+  
+    
+        
+      
+    
