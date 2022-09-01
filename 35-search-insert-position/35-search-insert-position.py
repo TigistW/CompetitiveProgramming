e1ed766591235258418f3457a4213ad4025bd222
TigistW <88367517+TigistW@users.noreply.github.com>
2022-09-01 23:28:59 +0300

    Time: 74 ms (8.26%), Space: 14 MB (78.19%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,30 +1,30 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        start = 0
-        end = len(nums) - 1 
-        index = 0
+class Solution(object):
+    def searchInsert(self, nums, target):
+      left = 0
+      right = len(nums) - 1
+      best = -1
+      while left <= right:
+        mid = left + (right - left) // 2
+        best = mid
+        # print(left,mid, right, nums[mid])
+        if nums[mid] < target:
+          # print("here")
+          left = mid + 1
+          # print(left)
+
+        elif nums[mid] > target:
+          right = mid - 1
+
+        else:
+          return best
         
-        while start <= end:
-          mid = start + (end - start) // 2
-          
-#           print("start", start,"middle",mid,"end",end)
-          
-          if nums[mid] == target:
-            index = mid
-            return index
-            
-          if nums[mid] < target:
-            index = mid + 1
-            start = mid + 1
-          
-          if nums[mid] > target:
-            index = mid
-            end = mid - 1
-        return index
-            
-          
-            
+      if nums[best] < target:
+        return best + 1
+      return best
+
+
+
+
+
         
-            
-          
         
