8fd99b8279d846d2d100d8b95d694f09b719622c
TigistW <88367517+TigistW@users.noreply.github.com>
2022-09-14 22:30:20 +0300

    Time: 138 ms (19.22%), Space: 13.8 MB (33.86%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,19 +1,40 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ls= []
-        dictt = {}
-        result = []
-        for i in nums2:
-            while ls and ls[-1] < i:
-                dictt[ls.pop()] = i
-            ls.append(i)
-        for i in nums1:
-            if i in dictt:
-                result.append(dictt[i])
-            else:
-                result.append(-1)
-        return result
-    
-    
-    
-    
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        dic =dict()
+        ans = [-1] * len(nums1)
+        
+        for i in range(len(nums1)):
+          dic[nums1[i]] = i
+          
+        for i in range(len(nums2)):
+          if nums2[i] not in dic:
+            continue
+            
+          for j in range(i, len(nums2)):
+            if nums2[j] > nums2[i]:
+              ans[dic[nums2[i]]]=nums2[j]
+              break
+        return ans
+              
+            
+          
+          
+          
+          
+              
+            
+            
+            
+              
+              
+              
+              
+            
+            
+            
+        
