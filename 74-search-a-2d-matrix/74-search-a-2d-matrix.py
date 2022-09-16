42de99fbe9aef960713163518c1b83a0183e5d7b
TigistW <88367517+TigistW@users.noreply.github.com>
2022-09-16 15:57:12 +0300

    Time: 79 ms (38.53%), Space: 14.5 MB (43.12%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,44 +1,32 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # for i in matrix:
-        #     for j in i: 
-        #         if j==target:
-        #             return True
-        # return False
-        
-        row=len(matrix)
-        col=len(matrix[0])     
         left = 0
-        right = (row * col) - 1
+        right = len(matrix) - 1
         
-        while(left <= right):
+        while left <= right:
+          mid = (left + right) // 2
+          print(left,mid,right)
+          if target < matrix[mid][0]:
+            # print("to the left")
+            right = mid - 1
+          elif target > matrix[mid][-1]:
+            # print("to the right")
+            left = mid + 1
+          elif target >= matrix[mid][0] and target <= matrix[mid][-1]:
+            # print("second search")
+            begin = 0
+            end = len(matrix[mid]) - 1
             
-            mid = left + (right - left) // 2
-
-            count , midrow, midcol = 0 , 0 , 0
-            for i in range(row): 
-                for j in range (col):
-                  count += 1
-                  if count - 1 == mid:
-                      midrow = i
-                      midcol = j
-
-            num = matrix[midrow][midcol]       
-            
-            # print("mid=",mid,"midcol=",midcol,"midrwo=",midrow,"num",num)
-            
-            if num==target:
+            while begin <= end:
+              middle = (begin + end) // 2
+              if matrix[mid][middle] == target:
                 return True
-            else:
-                if target > num:
-                    left = mid + 1 
-                else:
-                    right = mid - 1
-
+              elif matrix[mid][middle] > target: 
+                end = middle - 1
+              else:
+                begin = middle + 1
                 
-                
-                
-                
+            return False
             
             
-        
+            
