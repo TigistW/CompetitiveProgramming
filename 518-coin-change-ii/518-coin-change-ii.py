532424da835d21f9b9ab51d5b734460fbde99d0f
TigistW <88367517+TigistW@users.noreply.github.com>
2022-09-28 20:57:08 +0300

    Time: 275 ms (77.17%), Space: 14.1 MB (74.23%) - LeetHub

---
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0 for i in range(amount+1)]
+        dp[0] = 1
+        for c in coins:
+          for i in range(c,amount + 1):
+            dp[i] += dp[i - c]
+        return dp[-1]
+          
