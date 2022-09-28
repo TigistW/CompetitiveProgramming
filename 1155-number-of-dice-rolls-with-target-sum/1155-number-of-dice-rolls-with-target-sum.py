c8912b5b82c68962b5288a25c0fcfdd4de4279ae
TigistW <88367517+TigistW@users.noreply.github.com>
2022-09-29 02:19:04 +0300

    Time: 309 ms (86.89%), Space: 20.8 MB (17.24%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,15 +1,11 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-     # memo = {}
         @lru_cache(None)
-        def dp(d, target):
-            if d == 0:
+        def dp(n, target):
+            if n == 0:
                 return 0 if target > 0 else 1
-            # if (d, target) in memo:
-            #     return memo[(d, target)]
-            to_return = 0
-            for m in range(max(0, target-k), target):
-                to_return += dp(d-1, m)
-            # memo[(d, target)] = to_return
-            return to_return
+            val = 0
+            for m in range(max(0, target - k), target):
+                val += dp(n-1, m)
+            return val
         return dp(n, target) % (10**9 + 7)
