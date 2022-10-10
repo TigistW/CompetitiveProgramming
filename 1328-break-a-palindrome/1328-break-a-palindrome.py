b8b8dc64d236fe77398bc67d87274ecb90e574fe
TigistW <88367517+TigistW@users.noreply.github.com>
2022-10-10 08:28:50 +0300

    Time: 50 ms (57.66%), Space: 13.9 MB (61.62%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -4,21 +4,15 @@ class Solution:
         
         if len(palindrom) == 1:
             return ""
-        for i in palindrom:
-            lis_pali.append(i)
-        
+        lis_pali = list(palindrom)        
         for i in range(len(lis_pali)):
-            
             if lis_pali[i] != "a":
                     if i != len(lis_pali) - 1:
                         if i != (len(lis_pali) // 2):
                             lis_pali[i] = "a"
-                            break
-                        
+                            break             
             elif lis_pali[i] == "a" and i == len(lis_pali) - 1:
                 lis_pali[i] = chr(ord(lis_pali[i]) + 1)
                 break
-                
-                
         return ''.join (lis_pali)
                  
