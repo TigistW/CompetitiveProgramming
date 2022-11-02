5bdecbdc66395f1115c16036787c6ef1a0adf3b6
TigistW <88367517+TigistW@users.noreply.github.com>
2022-11-02 16:34:48 +0300

    Time: 58 ms (41.86%), Space: 13.9 MB (37.43%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        queue = deque()
+        queue.append(start)
+        components = ["A", "T", "G", "C"]
+        ops = 0
+        visited = set()
+        while queue:
+          print(queue)
+          for i in range(len(queue)):
+            cur = queue.popleft()
+            if cur == end:
+                return ops
+            else:
+              for idx in range(len(start)):
+                for i in components:
+                  cur_changed = cur[:idx] + i + cur[idx + 1:]
+                  if cur_changed in bank and cur_changed not in visited:
+                    queue.append(cur_changed)
+                    visited.add(cur_changed)
+          ops += 1
+        return -1        
