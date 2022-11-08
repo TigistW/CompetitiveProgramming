c0008c27854dc7f88d59b073ea61d6713ba319ad
TigistW <88367517+TigistW@users.noreply.github.com>
2022-11-08 07:07:48 +0300

    Time: 29 ms (99.57%), Space: 13.9 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,28 +3,15 @@ class Solution:
         stack = []
         
         for i in s:
-          if len(stack) == 0: 
+          if not stack:
             stack.append(i)
           else:
-            if i.islower():
-              if stack[-1].isupper():
-                if stack[-1].lower() == i:
-                  stack.pop()
-                else:
-                  stack.append(i)
-              else:
-                stack.append(i)
-                
+            if not i == stack[-1] and (i.lower() == stack[-1] or stack[-1].lower() == i):
+              stack.pop()
             else:
-              if stack[-1].islower():
-                if stack[-1].upper() == i:
-                  stack.pop()
-                else:
-                  stack.append(i)
-              else:
-                stack.append(i)
-    
-        return "".join(stack) 
+              stack.append(i)
+        return "".join(stack)
+          
               
               
             
