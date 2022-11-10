a90f7185ac15d6d60a55ef22eb1afe99380602cc
TigistW <88367517+TigistW@users.noreply.github.com>
2022-11-10 09:57:56 +0300

    Time: 1890 ms (93.44%), Space: 103.6 MB (86.39%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,38 @@
+class UnionFind:
+  def __init__(self, n):
+    self.parent = [i for i in range(n)]
+    self.rank = [1 for i in range(n)]
+    
+  def find(self, node):
+    while node != self.parent[node]:
+      node = self.parent[node]
+    return node
+  def union(self, node_one, node_two):
+    root_one = self.find(node_one)
+    root_two = self.find(node_two)
+    
+    if root_one != root_two:
+      if self.rank[root_one] < self.rank[root_two]:
+        self.parent[root_one] = root_two
+      elif self.rank[root_one] > self.rank[root_two]:
+        self.parent[root_two] = root_one
+      else:
+        self.parent[root_one] = root_two
+        self.rank[root_two] += 1
+        
+  def validPath(self, begin, end):
+    return self.find(begin) == self.find(end)
+    
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
+        uf = UnionFind(n)
         
         for i, j in edges:
-          graph[i].append(j)
-          graph[j].append(i)
+          uf.union(i, j)
+        return uf.validPath(source, destination)
           
-        stack = []
-        stack.append(source)
-        visited = set()
-        
-        while stack:
-          cur = stack.pop()
-          if cur == destination:
-            return True
-          # print("cur", cur)
-          if cur not in visited:
-            for child in graph[cur]:
-              stack.append(child)
-          visited.add(cur)
-        return False
+
             
             
             
