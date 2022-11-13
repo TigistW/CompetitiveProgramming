ff034e048d7a42b275412c73f654ea0af922fce1
TigistW <88367517+TigistW@users.noreply.github.com>
2022-11-13 10:27:24 +0300

    Time: 82 ms (89.18%), Space: 14.3 MB (15.65%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,26 +1,26 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
       def inBound(row, col):
-        return 0 <= row < len(image) and 0 <= col < len(image[0]) and image[row][col] == start_color
+        return 0 <= row < len(image) and 0 <= col < len(image[0]) and image[row][col] == start_color and (row, col) not in seen
       dxn = [(0,1), (1, 0), (-1, 0), (0, -1)] 
-      stack = []
       start_color = image[sr][sc]
-      stack.append((sr, sc))
       seen = set()
-      while stack:
-        row, col = stack.pop()
-        if (row, col) not in seen:
-          if image[row][col] == start_color:
-            image[row][col] = newColor
+      def dfs(i, j):
+        if image[i][j] == start_color:
+          image[i][j] = newColor
+        
+        seen.add((i, j))
+        for d in dxn:
+          new_row = d[0] + i
+          new_col = d[1] + j
+          
+          if inBound(new_row, new_col):
+            dfs(new_row, new_col)
             
-            seen.add((row, col))
-            for d in dxn:
-              new_row = row + d[0]
-              new_col = col + d[1]
-
-              if inBound(new_row, new_col):
-                stack.append((new_row, new_col))
+      dfs(sr, sc)
       return image
+        
+      
             
             
       
