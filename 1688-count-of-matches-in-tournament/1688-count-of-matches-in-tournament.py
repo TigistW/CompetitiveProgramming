23c8f280f4e12ef34dde1fe39a772fda28635d6e
TigistW <88367517+TigistW@users.noreply.github.com>
2022-12-22 21:31:49 +0300

    Time: 34 ms (83.96%), Space: 13.8 MB (95.52%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while  n > 1:
+          if n % 2 == 0:
+            matches += n// 2
+            n /= 2
+          else:
+            matches  += n // 2
+            n = (n //2 ) + 1
+        return int(matches)
+          
+          
+          
