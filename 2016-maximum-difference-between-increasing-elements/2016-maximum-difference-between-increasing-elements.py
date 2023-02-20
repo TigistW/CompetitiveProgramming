a9a167c00644804d0a458101e64a9bab9b0cd635
TigistW <88367517+TigistW@users.noreply.github.com>
2023-02-20 22:32:05 +0300

    Time: 345 ms (16.03%), Space: 14 MB (29.01%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -3,8 +3,9 @@ class Solution:
         val = -math.inf
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                if nums[j] - nums[i] > val and i < j and nums[i] < nums[j]:
-                    val = nums[j] - nums[i]
+                if i < j:
+                    if nums[j] - nums[i] > val and  nums[i] < nums[j]:
+                        val = nums[j] - nums[i]
         if val == - math.inf:
             return -1
         return val
